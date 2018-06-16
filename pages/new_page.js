9207b459ad8381104f3fde547d2417b1381f47e4
ARJITA CHAURASIA <arjch0302@gmail.com>
2018-06-16 06:45:05 +0530

    Add files via upload

---
+++ pages/new_page.js
@@ -0,0 +1,16 @@
+import Meta from "../components/Meta";
+
+import Start from "../components/Start";
+const Index = () => {
+    return (
+        <div>
+            <Meta />
+            {/*<Navbar />*/}
+            
+            <Start/>
+            
+        </div>
+
+    )
+}
+export default Index;
