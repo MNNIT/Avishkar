7b8171f6204ad6e02c69f3ea85d1193f69a2bdac
ARJITA CHAURASIA <arjch0302@gmail.com>
2018-06-28 20:35:11 +0530

    component files added

---
+++ components/MetaEvent.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+export default () => (
+	<div>
+		<Head>
+			<meta charset="UTF-8" />
+			<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<title>Avishkar Events</title>
+			<meta name="description" content="Avishkar: A techno management fest of MNNIT ALLAHABAD" />
+			<meta name="keywords" content="Avishkar, fest of MNNIT, techno management fest, technical event in MNNIT" />
+			<link rel="shortcut icon" href="favicon.ico" />
+			<link rel="stylesheet" type="text/css" href="static/css/demo.css" />
+			<link rel="stylesheet" type="text/css" href="static/css/component.css" />
+		</Head>
+	</div>
+);
