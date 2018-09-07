3102911638252de20adfb03a79418e4cc53fafd3
Poorna Chandu Gade <gadechandu123@gmail.com>
2018-09-07 16:32:59 +0530

    add page title

--- components/events/Meta.js
+++ components/events/Meta.js
@@ -5,6 +5,7 @@ export default ({ color }) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
+        <title>Avishkar 2018 | MNNIT Allahabad </title>
         <meta name="theme-color" content={color} />
         <link
           href="https://fonts.googleapis.com/css?family=Open+Sans"
