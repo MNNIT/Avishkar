789ca7327ab2a24a49d51b1f2d63159fc6a81354
Sumanth Reddy <srksumanth@gmail.com>
2019-04-30 16:09:26 +0530

    Update netlify.js

--- netlify.js
+++ netlify.js
@@ -1,5 +1,5 @@
 var fs = require("fs");
-const fileContent = `https://avishkar.mnnit.ac.in/* https://www.avishkarmnnit.in/:splat 301!`;
+const fileContent = `https://www.avishkarmnnit.in/* https://avishkar.mnnit.ac.in/:splat 301!`;
 fs.writeFile("out/_redirects", fileContent, function(err) {
   if (err) {
     return console.log(err);
